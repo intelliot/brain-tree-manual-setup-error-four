62d0151be5d14bb836fc480316087da90102586a
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-03-27 15:10:14 +1100

    Fix

--- BrainTreeObjC_four/ViewController.m
+++ BrainTreeObjC_four/ViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  Braintree *braintree = [Braintree braintreeWithClientToken:responseObject[@"client_token"]];
+  Braintree *braintree = [Braintree braintreeWithClientToken:@"client_token"];
 }
 
 - (void)didReceiveMemoryWarning {
